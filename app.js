296345b6df7e9aff72457cd2f618b1073bd12748
JorgeCarlotto <jorgemcarlotto@gmail.com>
2021-09-04 19:55:17 -0300

    new route index.html

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const publicFolderPath = path.resolve(__dirname, './public');
 app.use(express.static(publicFolderPath));
 
 //Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'))
+});
+
 app.get('/product-details', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/productDetail.html'))
 });
