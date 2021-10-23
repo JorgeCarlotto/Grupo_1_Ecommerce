468a837ace771b8d86425c62547166e1dc82fca7
Franco C Sanchez <francocsanchez@gmail.com>
2021-10-23 17:20:37 -0300

    Create Product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,8 @@
+//Require
 const path = require('path')
 const fs = require('fs')
 
+//Database JSON
 const productsFilePath = path.join(__dirname, '../data/productDataBase.json');
 const categoryProductsFilePath = path.join(__dirname, '../data/categoryDataBase.json');
 
@@ -14,13 +16,32 @@ let productController = {
         })
     },
     create: function (req, res) {
-        console.log(categoryProducts);
         res.render('product/create', {
             categoryProducts: categoryProducts
         })
     },
+    store: function (req, res) {
+        let product
+        let img
+
+        if (req.file != undefined) {
+            img = req.file.filename
+        } else {
+            img = 'product-default.png'
+        }
+
+        let ids = products.map(p => p.id)
+
+        let newProduct = {
+            id: Math.max(...ids) + 1,
+            ...req.body,
+            img: img
+        };
+        products.push(newProduct)
+        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, ' '));
+        res.redirect('/products');
+    },
     edit: function (req, res) {
-        console.log(categoryProducts);
         let product = products.find(product => product.id == req.params.id);
         res.render('product/edit', {
             product: product,
--- routers/product.js
+++ routers/product.js
@@ -1,11 +1,30 @@
+//Require
 let express = require('express');
 let productController = require('../controllers/productController.js');
+const path = require('path');
+const multer = require('multer');
 
 let router = express.Router();
+
+//Config multer
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname,'../public/img/products'))
+    },
+    filename: function (req, file, cb) {
+        const newFileName = 'product-' + Date.now() + path.extname(file.originalname);
+        cb(null, newFileName)
+    }
+})
+
+const upload = multer({
+    storage: storage
+})
 
 router.get('/', productController.index);
 //Routes CRUD
 router.get('/create', productController.create);
+router.post('/create',upload.single('img') ,productController.store);
 router.get('/show/:id', productController.show);
 router.get('/edit/:id', productController.edit);
 router.put('/edit/:id/update', productController.update);
