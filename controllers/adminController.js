4bc2bac19abf038cb090f59ced728db7ce921831
Franco C Sanchez <francocsanchez@gmail.com>
2021-11-22 23:49:13 -0300

    adminController

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,15 +1,13 @@
-const path = require('path');
-const fs = require('fs');
-
-const commenstsFilePath = path.join(__dirname, '../data/commentsDataBase.json');
-const commenst = JSON.parse(fs.readFileSync(commenstsFilePath, 'utf-8'));
+const db = require('../src/database/models');
+const sequelize = db.sequelize;
 
 let mainController = {
     admin: function (req,res){
         res.render('admin/adminPanel');
     },
     adminProducts: function(req, res){
-        res.render('admin/adminProducts');
+        db.Products.findAll()
+        .then(productos => console.log(productos));
     }
 };
 
