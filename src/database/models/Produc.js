4bcdd6f7047bfe8fafc74f3cc33b363308b25399
Franco C Sanchez <francocsanchez@gmail.com>
2021-11-22 21:23:14 -0300

    Module Product

---
+++ src/database/models/Produc.js
@@ -0,0 +1,44 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'Product';
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: dataTypes.STRING(100),
+            allowNull: false
+        },
+        description: {
+            type: dataTypes.STRING,
+            allowNull: false
+        },
+        stock: {
+            type: dataTypes.INTEGER,
+            allowNull: false
+        },
+        price: {
+            type: dataTypes.FLOAT,
+            allowNull: false
+        },
+        status: {
+            type: dataTypes.BOOLEAN,
+            defaultValue: true
+        },
+        category_id: {
+            type: dataTypes.INTEGER,
+            allowNull: false
+        },
+        flavor_id: {
+            type: dataTypes.INTEGER
+        }
+    };
+    let config = {
+        tableName: 'products',
+        timestamps: true
+    };
+    const Product = sequelize.define(alias, cols, config)
+
+    return Product
+}
