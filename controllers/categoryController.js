577d6dfdd8688d91e659ce6e39d547cf52c26d59
Franco C Sanchez <francocsanchez@gmail.com>
2021-11-24 22:13:32 -0300

    Category CRU

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,14 +1,59 @@
-const { render } = require('ejs');
+const {
+    render
+} = require('ejs');
 const db = require('../src/database/models');
 
 let categoryController = {
-    list: function (req,res){
+    list: function (req, res) {
         db.Category
-        .findAll()
-        .then(categories => res.render('admin/category/list',{categories}))
+            .findAll()
+            .then(categories => res.render('admin/category/list', {
+                categories
+            }))
     },
-    create: function(req,res){
+    create: function (req, res) {
         res.render('admin/category/create');
+    },
+    edit: function (req, res) {
+        db.Category
+            .findByPk(req.params.id)
+            .then(category => res.render('admin/category/edit', {
+                category
+            }));
+    },
+    store: function (req, res) {
+        db.Category
+            .create({
+                name: req.body.name
+            })
+            .then(() => res.redirect('/admin/categories'))
+    },
+    update: function (req, res) {
+        db.Category
+            .update({
+                name: req.body.name
+            }, {
+                where: {
+                    id: req.params.id
+                }
+            })
+            .then(() => res.redirect('/admin/categories'))
+    },
+    delete: function (req, res) {
+        db.Category
+            .findByPk(req.params.id)
+            .then(category => res.render('admin/category/delete', {
+                category
+            }));
+    },
+    destroy: function (req, res) {
+        db.category
+            .destroy({
+                where: {
+                    id: req.params.id
+                }
+            })
+            .then(() => res.redirect('/admin/categories'))
     }
 };
 
--- routers/category.js
+++ routers/category.js
@@ -7,5 +7,10 @@ const categoryController = require('../controllers/categoryController');
 //Panel de administracion
 router.get('/admin/categories', categoryController.list);
 router.get('/admin/categories/create', categoryController.create);
+router.post('/admin/categories/create', categoryController.store);
+router.get('/admin/categories/:id/edit', categoryController.edit);
+router.put('/admin/categories/:id/edit', categoryController.update);
+router.get('/admin/categories/:id/delete', categoryController.delete);
+router.delete('/admin/categories/:id/delete', categoryController.destroy);
 
 module.exports = router;
